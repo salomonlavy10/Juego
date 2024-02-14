25025a01bb4d5bdf7c3068b6cb997195ccbd1758
salomonlavy10 <142186128+salomonlavy10@users.noreply.github.com>
2024-02-14 18:23:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ $(document).keypress(function() {
     }
   });
 
+$("#level-title").click(function(){
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+      } 
+}
+);
+
 $('.btn').click(function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
